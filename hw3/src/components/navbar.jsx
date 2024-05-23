f7885613bf9ba3e7945ee5e5e8ead021c6322578
Moses Karemera <mkaremer@pdx.edu>
2024-05-22 18:01:26 -0700

    added customed page + route

--- hw3/src/components/navbar.jsx
+++ hw3/src/components/navbar.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="h-full w-64 bg-gray-900 text-white flex flex-col">
-      <div className="flex items-center justify-center bg-blue-500 p-4">
+    <nav className="h-screen w-64 bg-gray-900 text-white flex flex-col">
+      <div className="flex items-center justify-center bg-blue-700 p-4">
         <h1 className="text-lg font-bold">Homework 3</h1>
       </div>
       <ul className="space-y-2 text-lg mt-8">
@@ -24,7 +24,7 @@ const Navbar = () => {
         </li>
         <li className="hover:bg-gray-800 hover:text-white p-2 rounded">
           <Link to="/CustomRoute" className="flex items-center p-2">
-            <span className="ml-4">Custom-Route</span>
+            <span className="ml-4">Country GDP</span>
           </Link>
         </li>
       </ul>
--- hw3/src/pages/customRoute.jsx
+++ hw3/src/pages/customRoute.jsx
@@ -1,11 +1,92 @@
 import Navbar from "../components/navbar";
+import { useEffect, useState } from "react";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Header = () => {
   return (
     <div className="text-center mt-10">
-      <p className="text-5xl font-bold font-sans text-blue-600">
-        I need to comeup with custom route here!!
-      </p>
+      <h1 className="text-5xl font-bold font-sans text-blue-600">
+        The GDP for Countries in South America
+      </h1>
+    </div>
+  );
+};
+
+const GetGdp = () => {
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [],
+  });
+
+  useEffect(() => {
+    const fetchCountriesGdp = async () => {
+      try {
+        const res = await fetch(
+          "https://cs464p564-frontend-api.vercel.app/api/countries"
+        );
+        const data = await res.json();
+
+        const labels = data.map((country) => country.name);
+        const gdp = data.map((country) => country.gdp_billions);
+
+        setChartData({
+          labels,
+          datasets: [
+            {
+              label: "GDP (billions)",
+              data: gdp,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.6)",
+                "rgba(54, 162, 235, 0.6)",
+                "rgba(255, 205, 86, 0.6)",
+                "rgba(75, 192, 192, 0.6)",
+                "rgba(153, 102, 255, 0.6)",
+                "rgba(255, 159, 64, 0.6)",
+                "rgba(255, 0, 0, 0.6)",
+                "rgba(0, 255, 0, 0.6)",
+                "rgba(0, 0, 255, 0.6)",
+                "rgba(128, 128, 0, 0.6)",
+                "rgba(128, 0, 128, 0.6)",
+                "rgba(0, 128, 128, 0.6)",
+                "rgba(255, 128, 0, 0.6)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 205, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+                "rgba(255, 0, 0, 1)",
+                "rgba(0, 255, 0, 1)",
+                "rgba(0, 0, 255, 1)",
+                "rgba(128, 128, 0, 1)",
+                "rgba(128, 0, 128, 1)",
+                "rgba(0, 128, 128, 1)",
+                "rgba(255, 128, 0, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchCountriesGdp();
+  }, []);
+
+  return (
+    <div className="bg-gray-100 flex flex-col justify-center items-center h-screen p-10">
+      {chartData.labels.length > 0 && (
+        <div className="w-full lg:w-[800px] lg:h-[600px] mt-10">
+          <Pie data={chartData} options={{ responsive: true }} />
+        </div>
+      )}
     </div>
   );
 };
@@ -13,9 +94,12 @@ const Header = () => {
 const CustomRoute = () => {
   return (
     <div className="flex h-screen">
-      <Navbar />
+      <div>
+        <Navbar />
+      </div>
       <div className="flex-grow p-8 bg-gray-100">
         <Header />
+        <GetGdp />
       </div>
     </div>
   );
